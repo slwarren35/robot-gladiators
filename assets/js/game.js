5489b25189a075f0b23a1de209647a85c1c2cfa0
Sheri Warren <swarren35@yahoo.com>
2021-09-05 20:07:06 -0400

    starting functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("This is an alert! JavaScript is running!");
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// prompt for user to enter robot name
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+
+console.log("This is a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+
+// fight();
